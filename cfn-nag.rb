e7ab92bcbe493a21898cbd931644fc9b89d68fbc
KLEstes <klestes@gmail.com>
2019-07-14 03:55:50 -0500

    Update for version 0.4.33

--- cfn-nag.rb
+++ cfn-nag.rb
@@ -1,48 +1,39 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CfnNag < Formula
 
   desc "Auditing tool for CloudFormation templates"
   homepage "https://github.com/stelligent/cfn_nag"
-  url "https://github.com/stelligent/cfn_nag/tarball/v0.4.27"
-  version "0.4.27"
-  sha256 "3f41211f59f2029bc1f0b81e5ba3fa1210e010ed7eaeb3bdd7fe463b3acee3a3"
+  url "https://github.com/stelligent/cfn_nag/tarball/v0.4.33"
+  version "0.4.33"
+  sha256 "8a430d2869adbfe8ba726f0aad5d595972980c858a07cc09632ec1d50618f3d8"
 
+  
   depends_on "ruby" if MacOS.version <= :sierra
   
-
-  #s.add_runtime_dependency('cfn-model', '0.4.0')
   resource "cfn-model" do
     url "https://rubygems.org/gems/cfn-model-0.4.0-.gem"
     sha256 ""
   end
 
-  #s.add_runtime_dependency('colorize', '0.8.1')
   resource "colorize" do
     url "https://rubygems.org/gems/colorize-0.8.1.gem"
     sha256 "0ba0c2a58232f9b706dc30621ea6aa6468eeea120eb6f1ccc400105b90c4798c"
   end
 
-  #s.add_runtime_dependency('jmespath', '~> 1.3.1')
   resource "jmespath" do
     url "https://rubygems.org/gems/jmespath-1.4.0.gem"
     sha256 "ee5930ed833d3472fce31abf2f4a39852718e90b099dc2a6713234d600539cb4"
   end
 
-  #s.add_runtime_dependency('logging', '~> 2.2.2')
   resource "logging" do
     url "https://rubygems.org/gems/logging-2.2.2.gem"
     sha256 "963359dbdab725a3320eab179017d20d5b9731d7148e1efa8432c184a48a461a"
   end
 
-  #s.add_runtime_dependency('netaddr', '~> 1.5.1')
   resource "netaddr" do
     url "https://rubygems.org/gems/netaddr-2.0.3.gem"
     sha256 "e8e9b4c523d8597725bcaec133c2f7e588c4fe6f6695edfac271bd549f9bcbd5"
   end
 
-  #s.add_runtime_dependency('trollop', '~> 2.1.2')
   resource "trollop" do
     url "https://rubygems.org/gems/trollop-2.9.9.gem"
     sha256 "e4da3b29c084af29dc0d802dbbba0231fc3ee855e5ef57680b6f8aa32d3d901c"
